9b8beaedc5d2bb5e02bb81033331652ad3ca1208
FreddySeydon <renjaschmakeit@gmail.com>
2023-08-14 10:32:12 +0200

    Added lrcParser function in utils.

--- utils/lrcParser.js
+++ utils/lrcParser.js
@@ -1,5 +1,5 @@
-function parseLyrics(lrc) {
-    //separating time from lyrics
+export function parseLyrics(lrc) {
+    //matching time/text lines in lrc file and storing in group "time" and "text"
     const regex = /^\[(?<time>\d{2}:\d{2}(.\d{2})?)\](?<text>.*)/;
 
     //lrc string to individual lines
@@ -7,7 +7,26 @@ function parseLyrics(lrc) {
 
     const output = [];
 
+    //match lines that contain time and lyrics and store an object with each line in the output variable
     lines.forEach(line => {
         const match = line.match(regex)
-    })
+        if(match == null) return;
+
+        const {time, text} = match.groups;
+
+        output.push({
+            time: parseTime(time), //parsing the time to seconds with the function below
+            text: text.trim()
+        });
+    });
+
+    //parse separated time to seconds
+    function parseTime(time) {
+        const minsec = time.split(":")
+        const min = parseInt(minsec[0] * 60);
+        const sec = parseFloat(minsec[1]);
+    
+        return min + sec;
+    }
+    return output;
 }
