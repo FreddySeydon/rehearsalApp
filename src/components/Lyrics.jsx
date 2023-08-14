e3624bfce9d65093c120126d5afaecf6ad9b4455
FreddySeydon <renjaschmakeit@gmail.com>
2023-08-14 18:52:36 +0200

    Added conditional Styling depending on the currently active line.

--- src/components/Lyrics.jsx
+++ src/components/Lyrics.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import { parseLyrics, syncLyrics } from '../../utils/lrcParser';
+import OneLine from './OneLine';
 import "./Lyrics.css"
 
 const Lyrics = ({sounds, selectedTrack, setLrcContent, lrcContent, setLoading, loading, globalSeek, setGlobalSeek, setUserSeek, userSeek}) => {
     const [displayedLyrics, setDisplayedLyrics] = useState("")
     const [currentLyrics, setCurrentLyrics] = useState([])
+    const [displayedLyricsIndex, setDisplayedLyricsIndex] = useState(0);
+
     useEffect(() => {
         const loadLrc = async () => {
           setLoading(true)
@@ -22,6 +25,7 @@ const Lyrics = ({sounds, selectedTrack, setLrcContent, lrcContent, setLoading, l
         const lyrics = parseLyrics(lrcContent);
         setCurrentLyrics(lyrics);
         const index = syncLyrics(lyrics, globalSeek);
+        setDisplayedLyricsIndex(index);
         if(index == null) return;
         setDisplayedLyrics(lyrics[index].text);
 
@@ -42,15 +46,14 @@ const Lyrics = ({sounds, selectedTrack, setLrcContent, lrcContent, setLoading, l
         setGlobalSeek(position);
         setUserSeek(!userSeek);
     }
-    const testbool = true
-    //   loading ? null : 
+ 
   return (
     <div className="lyricsWrapper">
-        {loading ? <div>Loading...</div> : <div>{displayedLyrics}</div>}
+        {loading ? <div>Loading...</div> : <div style={{display: 'none'}}>{displayedLyrics}</div>}
         <div className="lyricsdisplay">
             Full Lyrics
             {currentLyrics.map((line, index) => {
-                return <div><a onClick={() => goToLyricsPosition(line.time)} style={{cursor: 'pointer', fontSize: testbool ? "1.5rem" : "5rem", fontWeight:'bold'}}>{line.text}</a> <br/></div>
+                return <div> <OneLine line={line} index={index} goToLyricsPosition={goToLyricsPosition} displayedLyricsIndex={displayedLyricsIndex} /> </div>
             })}
         </div>
     </div>
---
+++ src/components/OneLine.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState, useEffect, forwardRef } from 'react';
+
+const OneLine = ({line, index, displayedLyricsIndex, goToLyricsPosition}) => {
+    const testbool = true;
+    const [lineActive, setLineActive] = useState(false);
+    console.log(displayedLyricsIndex)
+    console.log(index)
+    console.log(lineActive)
+
+    useEffect(() => {
+        if(displayedLyricsIndex == null) {
+            setLineActive(false)
+            return
+        }
+        if(displayedLyricsIndex == index) {
+            setLineActive(true)
+            return
+        } setLineActive(false)
+    }, [displayedLyricsIndex])
+
+  return (
+    <div>
+      <a onClick={() => goToLyricsPosition(line.time)} style={{cursor: 'pointer', fontSize: "1.5rem", fontWeight: lineActive ? 'bold' : 'normal'}}>{line.text}</a>
+    </div>
+  )
+}
+
+export default OneLine
