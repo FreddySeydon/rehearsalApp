a70bfbc940a5e507bfcda2ea151fb0ee78150912
FreddySeydon <renjaschmakeit@gmail.com>
2024-06-23 23:11:02 +0200

    WIP fixing lyrics that don't load initially even if they are there.

--- src/components/Lyrics.jsx
+++ src/components/Lyrics.jsx
@@ -33,40 +33,46 @@ const Lyrics = ({
   const [displayedLyrics, setDisplayedLyrics] = useState("");
   const [currentLyrics, setCurrentLyrics] = useState([]);
   const [displayedLyricsIndex, setDisplayedLyricsIndex] = useState(0);
-  const [currentLrc, setCurrentLrc] = useState({})
+  const [currentLrc, setCurrentLrc] = useState({});
 
   const lyricsRef = useRef();
-  
 
   useEffect(() => {
-    console.log("Current: ",currentLrcs)
     const loadLrc = async () => {
-      setLoading(true);
-      if(currentLrcs.length === 0){
-        console.log("No lrc found")
-        setLoading(false);
-        return
-      }
-      const currentLrc = currentLrcs.find((lrc) => lrc.trackId === selectedTrack)
-      console.log("one:", currentLrc)
-      if(currentLrc){
-        const lrcLocation = currentLrc.lrc
-      const lrc = await lrcLocation.text();
-      setCurrentLrc(currentLrc);
-      setLrcContent(lrc);
-      setLoading(false);
-      return
-      }
-      setNoTrackLrc(true);
-      setLoading(false)
+      if (currentLrcs.length && selectedTrack) {
+        console.log("Current LRCs: ", currentLrcs);
+        setLoading(true);
 
+        if (currentLrcs.length === 0) {
+          console.log("No LRC found");
+          setLoading(false);
+          return;
+        }
+        console.log('Lrc.TrackId: ', currentLrcs[0].trackId);
+
+        const foundLrc = currentLrcs.find((lrc) => lrc.trackId === selectedTrack);
+        console.log("Found LRC: ", foundLrc);
+
+        if (foundLrc) {
+          const lrcLocation = foundLrc.lrc;
+          const lrcText = await lrcLocation.text();
+
+          setCurrentLrc(foundLrc);
+          setLrcContent(lrcText);
+        } else {
+          setNoTrackLrc(true);
+        }
+
+        setLoading(false);
+      }
     };
+
     loadLrc();
-    lyricsRef.current.scrollTo(0,0);
-  }, [selectedSong, selectedTrack]);
+    lyricsRef.current.scrollTo(0, 0);
+  }, [selectedSong, selectedTrack, currentLrcs, setLoading, setLrcContent, setNoTrackLrc]);
 
   const displayCurrentLyrics = () => {
-    if(lrcContent){
+    if (lrcContent) {
       const lyrics = parseLyrics(lrcContent);
       setCurrentLyrics(lyrics);
       const index = syncLyrics(lyrics, globalSeek);
@@ -77,17 +83,16 @@ const Lyrics = ({
   };
 
   useEffect(() => {
-    if (globalSeek == 0) {
+    if (globalSeek === 0) {
       setDisplayedLyrics("");
     }
     if (!loading) {
       displayCurrentLyrics();
     }
-    return;
   }, [globalSeek, loading]);
 
   const goToLyricsPosition = (position) => {
-    if(Transport.state === "started"){
+    if (Transport.state === "started") {
       Transport.pause();
       Object.values(statePlayers._players).forEach((player) => player.sync());
       Transport.seconds = position;
@@ -103,41 +108,36 @@ const Lyrics = ({
   };
 
   return (
-    <>
-      <div className="lyricsWrapper" style={{width: isTabletOrMobile ? "100%" : "25rem", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", marginLeft: isTabletOrMobile ? 0 : 0}}>
-        {/* <h3>Lyrics</h3> */}
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <div style={{ display: "none" }}>{displayedLyrics}</div>
-        )}
-        <div ref={lyricsRef} className="lyricsdisplay" style={{width: "100%", overflowY: 'scroll', scrollbarWidth: 'none'}}>
-          {currentLyrics.map((line, index) => {
-            return (
-              <div key={index}>
-                {" "}
-                <OneLine
-                  line={line}
-                  key={index}
-                  index={index}
-                  goToLyricsPosition={goToLyricsPosition}
-                  displayedLyricsIndex={displayedLyricsIndex}
-                  isBigScreen={isBigScreen}
-                  isDesktopOrLaptop={isDesktopOrLaptop}
-                  isTabletOrMobile={isTabletOrMobile}
-                />{" "}
-              </div>
-            );
-          })}
-        </div>
-        {!currentLrc.fullySynced ?         
-        <Link to={`/lyricseditor?albumId=${selectedAlbum}&songId=${selectedSong}&trackId=${selectedTrack}`}>
-          <p style={{textDecoration: "underline", margin: 0.5, color: 'whitesmoke'}}>Lyrics are not synced completely yet.</p>
-          {/* <p style={{textDecoration: "underline", margin: 0.5, color: 'whitesmoke'}}>Click here to go to Sync</p> */}
-        </Link> : null
-      }
+    <div className="lyricsWrapper" style={{ width: isTabletOrMobile ? "100%" : "25rem", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginLeft: isTabletOrMobile ? 0 : 0 }}>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div style={{ display: "none" }}>{displayedLyrics}</div>
+      )}
+      <div ref={lyricsRef} className="lyricsdisplay" style={{ width: "100%", overflowY: 'scroll', scrollbarWidth: 'none' }}>
+        {currentLyrics.map((line, index) => {
+          return (
+            <div key={index}>
+              <OneLine
+                line={line}
+                key={index}
+                index={index}
+                goToLyricsPosition={goToLyricsPosition}
+                displayedLyricsIndex={displayedLyricsIndex}
+                isBigScreen={isBigScreen}
+                isDesktopOrLaptop={isDesktopOrLaptop}
+                isTabletOrMobile={isTabletOrMobile}
+              />
+            </div>
+          );
+        })}
       </div>
-    </>
+      {!currentLrc.fullySynced ? (
+        <Link to={`/lyricseditor?albumId=${selectedAlbum}&songId=${selectedSong}&trackId=${selectedTrack}`}>
+          <p style={{ textDecoration: "underline", margin: 0.5, color: 'whitesmoke' }}>Lyrics are not synced completely yet.</p>
+        </Link>
+      ) : null}
+    </div>
   );
 };
 
